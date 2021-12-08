1a53f790f1b11674ee08f596617aba86cc39f6ca
DanielNery <danielpontesnery@gmail.com>
2021-12-08 19:58:27 -0300

    feat: Melhorando estrutura do servidor.

--- app.py
+++ app.py
@@ -2,34 +2,28 @@ from flask import Flask
 from flask_socketio import SocketIO
 from flask_cors import CORS, cross_origin
 
-import time
-import random
+from websocket.socket import FootprintWebSocket
+
 
 app = Flask(__name__)
 
 cors = CORS(app, resources={r"/foo": {"origins": "*"}})
+
 app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-io = SocketIO(app, cors_allowed_origins='*')
+def set_app(app: object):
+    """Faz a inicialização da aplicação
 
-@io.on('getData')
-def handle_getdata_event():
+    Args:
+        app (object): servidor flask
     """
-        Retona os dados para client
-    """
-    count = 0
-    while True:
-        count+=1
-        time.sleep(2)
-        msg = {"x": count, "y": random.randint(1, 100) }
-        io.emit('getData', msg)
+    footprint_websocket = FootprintWebSocket(app)
+    footprint_websocket.run_websocket()
 
-@io.on('newConnect')
-def handle_newconnect_event(json):
-    print("\n\n " + json['data'] + "\n\n")
-    handle_getdata_event()
+
 
 if __name__ == '__main__':
-    io.run(app, debug=False)
+    set_app(app)
+    
---
+++ websocket/socket.py
@@ -0,0 +1,49 @@
+from flask_socketio import SocketIO
+
+import time
+import random
+
+
+class FootprintWebSocket():
+
+    def __init__(self, app: object) -> object:
+        """Função construtora do serviço de websocket
+
+        Args:
+            app (object): servidor flask
+
+        Returns:
+            object: Instância do serviço
+        """
+        self.app = app
+
+
+    def run_websocket(self):
+        io = SocketIO(self.app, cors_allowed_origins='*')
+        
+        @io.on('getData')
+        def handle_getdata_event():
+            """
+                Retona os dados para client
+            """
+            count = 0
+            while True:
+                count+=1
+                time.sleep(2)
+                msg = {"x": count, "y": random.randint(1, 100) }
+                io.emit('getData', msg)
+
+        @io.on('newConnect')
+        def handle_newconnect_event(json):
+            print("\n\n " + json['data'] + "\n\n")
+            handle_getdata_event()
+
+
+
+        io.run(self.app, debug=True)
+
+
+
+
+
+    
